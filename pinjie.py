de66871efcca09ac1f22c3d1d96d99fc6864c558
zhonguochong <jxgxzgc@163.com>
2020-06-29 14:42:03 +0800

    remove some files

--- pinjie.py
+++
@@ -1,8 +0,0 @@
-'''
-@Author: your name
-@Date: 2020-06-23 16:06:11
-@LastEditTime: 2020-06-23 16:06:11
-@LastEditors: Please set LastEditors
-@Description: In User Settings Edit
-@FilePath: /leetcoding/pinjie.py
-'''
